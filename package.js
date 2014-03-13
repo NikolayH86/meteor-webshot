4c4e1c1586378b2f8bc04a5f83171a9bde34643c
NikolayH <nikolayh@interlink-ua.com>
2014-03-13 12:30:47 +0200

    Websot dependency version was updated.

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   summary: "Webshot - NPM Screenshot Library using PhantomJS, repackaged For Meteor"
 });
 
-Npm.depends({webshot: "0.5.0"});
+Npm.depends({webshot: "0.11.0"});
 
 Package.on_use(function (api) {
   api.add_files("lib/webshot.js", "server");
